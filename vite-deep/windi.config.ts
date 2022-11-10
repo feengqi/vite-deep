b8fa913f112360dbfe0f9d59c4adffd744b69213
feng <feengqi@gmail.com>
2022-11-10 13:28:03 +0800

    4、 样式方案：在 `Vite` 中接入现代化的 `CSS` 工程化方案 -- CSS 原子化框架 - Windi CSS 接入 - shortcuts

--- vite-deep/windi.config.ts
+++ vite-deep/windi.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from "vite-plugin-windicss";
 export default defineConfig({
   // 开启 attributify
   attributify: true,
+  // shortcuts 用来封装一系列的原子化能力，尤其是一些常见的类名集合
+  // 比如这里封装了flex-c的类名，接下来我们可以在业务代码直接使用这个类名:
+  // <div className="flex-c"></div>
+  shortcuts: {
+    "flex-c": "flex justify-center items-center",
+  }
 });
