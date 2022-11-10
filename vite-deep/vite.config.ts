7ffa1c53dcd853356ba3ba80f24f0b404f0f7873
feng <feengqi@gmail.com>
2022-11-10 13:06:49 +0800

    4、 样式方案：在 `Vite` 中接入现代化的 `CSS` 工程化方案 -- CSS 预处理器 - CSS Modules

--- vite-deep/vite.config.ts
+++ vite-deep/vite.config.ts
@@ -12,6 +12,11 @@ const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 export default defineConfig({
   plugins: [react()],
   css: {
+    modules: {
+      // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
+      // 其中，name 表示当前文件名，local 表示类名
+      generateScopedName: "[name]__[local]___[hash:base64:5]"
+    },
     preprocessorOptions: {
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
