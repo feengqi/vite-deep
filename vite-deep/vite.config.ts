f95452f3633eb9dd241edd719a20573c5cfdf815
feng <feengqi@gmail.com>
2022-11-10 13:15:49 +0800

    4、 样式方案：在 `Vite` 中接入现代化的 `CSS` 工程化方案 -- CSS 预处理器 - PostCSS

--- vite-deep/vite.config.ts
+++ vite-deep/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, normalizePath } from 'vite';
 // 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
 import path from 'path';
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer';
 
 // 全局 scss 文件的路径
 // 用 normalizePath 解决 window 下的路径问题
@@ -22,6 +23,14 @@ export default defineConfig({
         // additionalData 的内容会在每个 scss 文件的开头自动注入
         additionalData: `@import "${variablePath}";`
       }
+    },
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+        })
+      ]
     }
   }
 })
