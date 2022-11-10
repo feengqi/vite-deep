926844f57d2937100a7487991bb61057841495fa
feng <feengqi@gmail.com>
2022-11-10 12:44:18 +0800

    4、 样式方案：在 `Vite` 中接入现代化的 `CSS` 工程化方案 -- CSS 预处理器 - 使用sass和配置sass全局变量

---
+++ vite-deep/src/components/Header/index.tsx
@@ -0,0 +1,5 @@
+import './index.scss';
+
+export function Header() {
+  return <p className="header">This is Header</p>;
+}
--- vite-deep/vite.config.ts
+++ vite-deep/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+// vite.config.ts
+import { defineConfig, normalizePath } from 'vite';
+// 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
+import path from 'path';
 import react from '@vitejs/plugin-react'
+
+// 全局 scss 文件的路径
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`
+      }
+    }
+  }
 })
