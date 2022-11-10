5be4c0211cd9a16075e2d175ab30af89a0af2c01
feng <feengqi@gmail.com>
2022-11-10 13:20:24 +0800

    4、 样式方案：在 `Vite` 中接入现代化的 `CSS` 工程化方案 -- CSS 预处理器 - CSS In JS

--- vite-deep/vite.config.ts
+++ vite-deep/vite.config.ts
@@ -11,7 +11,25 @@ const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    // 加入 babel 插件
+    // 以下插件包都需要提前安装
+    // 当然，通过这个配置你也可以添加其它的 Babel 插件
+    babel: {
+      // 加入 babel 插件
+      // 以下插件包都需要提前安装
+      // 当然，通过这个配置你也可以添加其它的 Babel 插件
+      plugins: [
+        // 适配 styled-component
+        "babel-plugin-styled-components",
+        // 适配 emotion
+        "@emotion/babel-plugin"
+      ]
+    },
+    // 注意: 对于 emotion，需要单独加上这个配置
+    // 通过 `@emotion/react` 包编译 emotion 中的特殊 jsx 语法
+    jsxImportSource: "@emotion/react"
+  })],
   css: {
     modules: {
       // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
