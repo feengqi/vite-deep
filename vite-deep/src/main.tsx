2f827b9523cb5d6e5cbc4030e705c7157eea41e0
feng <feengqi@gmail.com>
2022-11-10 13:23:22 +0800

    4、 样式方案：在 `Vite` 中接入现代化的 `CSS` 工程化方案 -- CSS 原子化框架 - Windi CSS 接入

--- vite-deep/src/main.tsx
+++ vite-deep/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// 用来注入 Windi CSS 所需的样式，一定要加上！
+import "virtual:windi.css";
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
--- vite-deep/vite.config.ts
+++ vite-deep/vite.config.ts
@@ -2,8 +2,9 @@
 import { defineConfig, normalizePath } from 'vite';
 // 如果类型报错，需要安装 @types/node: pnpm i @types/node -D
 import path from 'path';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
+import windi from 'vite-plugin-windicss';
 
 // 全局 scss 文件的路径
 // 用 normalizePath 解决 window 下的路径问题
@@ -11,44 +12,47 @@ const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    // 加入 babel 插件
-    // 以下插件包都需要提前安装
-    // 当然，通过这个配置你也可以添加其它的 Babel 插件
-    babel: {
+  plugins: [
+    react({
       // 加入 babel 插件
       // 以下插件包都需要提前安装
       // 当然，通过这个配置你也可以添加其它的 Babel 插件
-      plugins: [
-        // 适配 styled-component
-        "babel-plugin-styled-components",
-        // 适配 emotion
-        "@emotion/babel-plugin"
-      ]
-    },
-    // 注意: 对于 emotion，需要单独加上这个配置
-    // 通过 `@emotion/react` 包编译 emotion 中的特殊 jsx 语法
-    jsxImportSource: "@emotion/react"
-  })],
+      babel: {
+        // 加入 babel 插件
+        // 以下插件包都需要提前安装
+        // 当然，通过这个配置你也可以添加其它的 Babel 插件
+        plugins: [
+          // 适配 styled-component
+          'babel-plugin-styled-components',
+          // 适配 emotion
+          '@emotion/babel-plugin',
+        ],
+      },
+      // 注意: 对于 emotion，需要单独加上这个配置
+      // 通过 `@emotion/react` 包编译 emotion 中的特殊 jsx 语法
+      jsxImportSource: '@emotion/react',
+    }),
+    windi()
+  ],
   css: {
     modules: {
       // 一般我们可以通过 generateScopedName 属性来对生成的类名进行自定义
       // 其中，name 表示当前文件名，local 表示类名
-      generateScopedName: "[name]__[local]___[hash:base64:5]"
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
     },
     preprocessorOptions: {
       scss: {
         // additionalData 的内容会在每个 scss 文件的开头自动注入
-        additionalData: `@import "${variablePath}";`
-      }
+        additionalData: `@import "${variablePath}";`,
+      },
     },
     postcss: {
       plugins: [
         autoprefixer({
           // 指定目标浏览器
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
-        })
-      ]
-    }
-  }
-})
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+        }),
+      ],
+    },
+  },
+});
